ee28505cd178c4a035821bdbb202e21351edb4e6
Petros Koutroulis <peterkoutroulis2004@gmail.com>
2026-07-07 03:05:12 +0300

    Update room calibration defaults

--- viewer/sceneCalibrations.js
+++ viewer/sceneCalibrations.js
@@ -3,7 +3,7 @@ const SCENE_CALIBRATION_DEFAULTS = {
     "indoors:classroom-5": {
       scene: {
         position: [0, 1.4, 0],
-        rotationDegrees: [0, 0, 0],
+        rotationDegrees: [180, -2, 0],
         scale: [1, 1, 1],
       },
       collision: {
@@ -14,6 +14,11 @@ const SCENE_CALIBRATION_DEFAULTS = {
       spawn: {
         position: [4.2, 1.6, -0.5],
         rotationDegrees: [0, 90, 0],
+      },
+      cameraStart: {
+        position: [-10.251, 6.432, -10.286],
+        rotationDegrees: [200, -44.8, 0],
+        scale: [1, 1, 1],
       },
     },
     "indoors:main-hall": {
@@ -115,7 +120,7 @@ const SCENE_CALIBRATION_DEFAULTS = {
     "indoors:metabolism": {
       scene: {
         position: [0, 0, 0],
-        rotationDegrees: [0, 0, 0],
+        rotationDegrees: [180, 0, 0],
         scale: [1, 1, 1],
       },
       collision: {
@@ -126,6 +131,11 @@ const SCENE_CALIBRATION_DEFAULTS = {
       spawn: {
         position: [0, 0.5, 2.2],
         rotationDegrees: [0, 0, 0],
+      },
+      cameraStart: {
+        position: [-7.152371406555176, 3.3036532402038574, -1.0008203983306885],
+        rotationDegrees: [168.00000126640109, -78.8875566105402, 179.99999991298583],
+        scale: [1, 1, 1],
       },
     },
     "indoors:kitchen": {
@@ -148,7 +158,7 @@ const SCENE_CALIBRATION_DEFAULTS = {
   manualBoxOverrides: {
     "indoors:classroom-5": {
       position: [0, -2.2, -0.3],
-      rotationDegrees: [89.5, -0.1, -450.4],
+      rotationDegrees: [89.6, -0.1, -449.4],
       scale: [9.2, 9.8, 4.4],
       cutRatio: 0.23,
       cutDepthByFace: {
@@ -157,6 +167,28 @@ const SCENE_CALIBRATION_DEFAULTS = {
         front: 0.19,
         back: 0.2,
         top: 0.19,
+        bottom: 0.23,
+      },
+      cutDepthLockedByFace: {
+        left: true,
+        right: true,
+        front: true,
+        back: true,
+        top: true,
+        bottom: true,
+      },
+    },
+    "indoors:metabolism": {
+      position: [0, -1.7, 0],
+      rotationDegrees: [90, 0, 178.7],
+      scale: [3.9, 5.7, 3.4],
+      cutRatio: 0.23,
+      cutDepthByFace: {
+        left: 0.19,
+        right: 0.17,
+        front: 0.19,
+        back: 0.27,
+        top: 0.23,
         bottom: 0.23,
       },
       cutDepthLockedByFace: {
